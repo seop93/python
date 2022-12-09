09552cf41861e5d112dc35cfb8c345cff1fadd3c
byeonheungseob <kevin9160@naver.com>
2022-12-09 09:35:31 +0900

    Input 사용법

---
+++ python/chapter05_02.py
@@ -0,0 +1,30 @@
+# Chapter05-2
+# 파이썬 사용자 입력
+# Input 사용법
+# 기본 타입(str)
+
+# # ex1
+# name = input("Enter Your Name")
+# grade = input("Enter Your Grade")
+# company = input("Enter Your Company name")
+#
+# print(name, grade, company)
+
+# ex2
+# number = input("Enter number: ")
+# name = input("Enter name: ")
+#
+# print("type of number", type(number), type(name))
+
+# ex3(계산)
+# first_number = int(input("Enter number1: "))
+# second_number = int(input("Enter number2: "))
+#
+# total = first_number + second_number
+# print("first_number + second_number: ", total)
+
+# ex4
+float_number = float(input("Enter a float number: "))
+
+print("input float: ", float_number)
+print("input type: ", type(float_number))
